6b9a842efa393a7af07d5ecd1b16ae018deef4b5
Blin Qipa <blinnqipa@gmail.com>
2020-03-02 17:54:49 +0300

    Added the Desert Assets

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.0f;
+
+    public Vector2 maxCameraPosition;
+    public Vector2 minCameraPosition;
+
+    void Update()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //targetPosition.x = Mathf.Clamp(targetPosition.x, minCameraPosition.x, maxCameraPosition.x);
+            //targetPosition.y = Mathf.Clamp(targetPosition.y, minCameraPosition.y, maxCameraPosition.y);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, speed);
+            
+            
+            
+        }
+    }
+}
