2ea77eda575dcfd0e72e0dec534692d2c458fba5
Blin Qipa <blinnqipa@gmail.com>
2020-05-01 05:05:08 +0300

    Added backgrounds, template level added
    Template level will be used as a base for building new levels. Although non functional for now, backgrounds have been imported.

---
+++ Assets/BackgroundFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour
+{
+    Camera cam;
+    [SerializeField]GameObject image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.transform.position = new Vector3(cam.transform.position.x,cam.transform.position.y,cam.transform.position.z);
+    }
+}
