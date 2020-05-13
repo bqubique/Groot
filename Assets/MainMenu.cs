0beeecbd1e00b71315d98650e1666ac58ffb5d70
hsnmrdgl <hcaglar56@gmail.com>
2020-05-13 22:41:22 +0300

    MainMenu
    +Added Menu to game.
    +Some bugs fixed.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    [Header("Menu Screens")]
+    public GameObject mainScreen;
+    public GameObject startScreen;
+    public GameObject newLevelScreen;
+    public GameObject loadLevelScreen;
+    
+
+	public void StartScreen()
+    {
+        mainScreen.SetActive(false);
+        startScreen.SetActive(true);
+    }
+
+    public void NewLevel()
+    {
+        startScreen.SetActive(false);
+        newLevelScreen.SetActive(true);
+        //Application.LoadLevel("FirstLevel");
+    }
+
+    public void LoadLevel()
+    {
+        startScreen.SetActive(false);
+        loadLevelScreen.SetActive(true);
+    }
+
+    public void back2Menu()
+    {
+        startScreen.SetActive(false);
+        loadLevelScreen.SetActive(false);
+        newLevelScreen.SetActive(false);
+        mainScreen.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("DENEME : EXIT SUCCESFULL");
+        Application.Quit();
+    }
+
+}
